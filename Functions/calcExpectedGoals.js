8b9719d924a9700a7bfbc630b03d0835524587e2
Gabi B <Gab3009@Gmail.com>
2019-07-16 15:49:10 +0300

    osher cant write code, fixed it

--- Functions/calcExpectedGoals.js
+++ Functions/calcExpectedGoals.js
@@ -4,24 +4,24 @@ const calcExpectedGoals = async function (league, homeTeam, awayTeam) {
     const allAvg = await dataModels[league].find({
         name: "allAvg"
     })
-
     const homeData = await dataModels[league].find({
         name: homeTeam,
         place: "Home"
     })
-    const homeAvgFor = homeData.avgFor / allAvg.avgFor
-    const homeAvgAgainst = homeData.avgAgainst / allAvg.avgAgainst
+    const homeAvgFor = homeData[0].avgFor / allAvg[0].avgFor
+    const homeAvgAgainst = homeData[0].avgAgainst / allAvg[0].avgAgainst
 
     const awayData = await dataModels[league].find({
         name: awayTeam,
         place: "Away"
     })
-    const awayAvgFor = awayData.avgFor / allAvg.avgAgainst
-    const awayAvgAgainst = awayData.avgAgainst / allAvg.avgFor
+
+    const awayAvgFor = awayData[0].avgFor / allAvg[0].avgAgainst
+    const awayAvgAgainst = awayData[0].avgAgainst / allAvg[0].avgFor
 
     return {
-        homeExpextedGoals: homeAvgFor * awayAvgAgainst * allAvg.avgFor,
-        awayExpextedGoals: awayAvgFor * homeAvgAgainst * allAvg.avgAgainst
+        homeExpextedGoals: homeAvgFor * awayAvgAgainst * allAvg[0].avgFor,
+        awayExpextedGoals: awayAvgFor * homeAvgAgainst * allAvg[0].avgAgainst
     }
 }
 module.exports = calcExpectedGoals

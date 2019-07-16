db110e58007c56b67bce76cfed58f615ac9f6ba2
Gabi B <Gab3009@Gmail.com>
2019-07-16 15:10:47 +0300

    hey

--- Functions/relevantTeams.js
+++ Functions/relevantTeams.js
@@ -2,13 +2,13 @@ const relevantTeams = function (leagueName) {
     const premierLeague = ["Man City", "Liverpool", "Chelsea", "Tottenham", "Arsenal", "Man United", "Wolves", "Everton", "Leicester", "West Ham", "Watford", "Crystal Palace", "Newcastle", "Bournemouth", "Burnley", "Southampton", "Brighton"]
     const seriaA = ['Juventus', 'Napoli', 'Spal', 'Atalanta', 'Inter', 'Milan', 'Roma', 'Torino', 'Lazio', 'Sampdoria', 'Bologna', 'Sassuolo', 'Udinese', 'Parma', 'Cagliari', 'Fiorentina', 'Genoa']
     const laLiga = ["Barcelona", "Ath Madrid", "Real Madrid", "Valencia", "Getafe", "Sevilla", "Espanol", "Ath Bilbao", "Sociedad", "Betis", "Alaves", "Eibar", "Leganes", "Villarreal", "Levante", "Valladolid", "Celta"]
-    if (leagueName === "premierLeague") {
+    if (leagueName === "Premier-Leauge") {
         return premierLeague
     }
-    if (leagueName === "seriaA") {
+    if (leagueName === "Seria-A") {
         return seriaA
     }
-    if (leagueName === "laLiga") {
+    if (leagueName === "La-Liga") {
         return laLiga
     }
 }

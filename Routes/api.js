ee64d2e3737194bc218e048d70cfff7048d4ffb4
Gabi B <Gab3009@Gmail.com>
2019-07-16 14:00:01 +0300

    fixed basic router problams

--- Routes/api.js
+++ Routes/api.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const request = require('request')
+const dataModel = require('../Mongoose/dataSchema')
 const router = express.Router()
 
 
 router.get('/try/:league' , function(req , res){
     const league = req.params.league
-    league.find({} , function(err ,res2){
+    dataModel[league].find({} , function(err ,res2){
         res.send(res2)
     })
 })

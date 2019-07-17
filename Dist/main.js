07ed9846de0fc5baa95034729f0189aa43c83b3f
Elad Ezra <xxeladxx@gmail.com>
2019-07-17 10:06:20 +0300

    some stuff

--- Dist/main.js
+++ Dist/main.js
@@ -3,7 +3,7 @@ $('body').on('click', '.league', function () {
     $('.input').empty()
     $('.name').html(`${$(this).text()}`)
     $('.input').append(`
-        <div class=form><input type=text id="homeTeam" class="bar" placeholder="Home Team"></div>
+        <div class=form>  <i class="fas fa-trash"></i><input type=text id="homeTeam" class="bar" placeholder="Home Team"></div>
         <div class=form><input type=text id="awayTeam" class="bar" placeholder="Against Team"></div>
         <div class=form><button class="button">Click</button></div>
     `)
@@ -17,9 +17,20 @@ $('body').on('click', '.button', function () {
         league: $(this).closest('.data').find('.name').text().replace(" ", "-")
     }
     $.post('/calc', data, function (res) {
-        for(let i of res){
-            let div = 
-            $('.data2').append(`<div class="chance">${i.chance.toFixed(5) + ' '} </div>`)
+        for (let i of res) {
+            let div =
+                $('.data2').append(`<div class="chance">${i.chance.toFixed(5) + ' '} </div>`)
         }
     })
+})
+
+$('body').on('keypress', '#awayTeam', function (event) { // I CANT PRESS ENTER AND IT WORKS
+    if (event.keyCode == 13) {
+        $('.button').click()
+    }
+})
+
+$('body').on('click' , '.fa-trash' ,function(){
+    $('#homeTeam').val('')
+    $('#awayTeam').val('')
 })

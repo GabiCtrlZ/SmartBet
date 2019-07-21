31762405b072be03c1c720bdbb3270258268323e
Osher Solimany <solimani.osher@gmail.com>
2019-07-21 15:15:39 +0300

    gfgdf

--- Functions/relevantTeams.js
+++ Functions/relevantTeams.js
@@ -1,4 +1,5 @@
 
+
 const premierLeague = ["Man City", "Liverpool", "Chelsea", "Tottenham", "Arsenal", "Man United", "Wolves", "Everton", "Leicester", "West Ham", "Watford", "Crystal Palace", "Newcastle", "Bournemouth", "Burnley", "Southampton", "Brighton"]
 const seriaA = ["Juventus", "Napoli", "Spal", "Atalanta", "Inter", "Milan", "Roma", "Torino", "Lazio", "Sampdoria", "Bologna", "Sassuolo", "Udinese", "Parma", "Cagliari", "Fiorentina", "Genoa"]
 const laLiga = ["Barcelona", "Ath Madrid", "Real Madrid", "Valencia", "Getafe", "Sevilla", "Espanol", "Ath Bilbao", "Sociedad", "Betis", "Alaves", "Eibar", "Leganes", "Villarreal", "Levante", "Valladolid", "Celta"]
